5b39a36316a8b5502218ca14a70e6c5c3ff1b4e2
jzywiecki <jakubzywiecki1@gmail.com>
2024-05-14 21:57:54 +0200

    Add risk module
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

---
+++ server/modules/riskModule/routes.py
@@ -0,0 +1,32 @@
+import utils.openaiUtils as utils
+import json
+import modules.module.module as modules
+
+actors_with_description_json = '''
+    "risks": [
+        {
+            "risk": "string",
+            "description": "string"
+        }
+    ]
+'''
+query_for_who = "Wskaż 10 technologicznych ryzyk systemu z krótkim opisem dla"
+query_doing_what = "tworzacego aplikacje do"
+query_expectations = "Skup się na celu startupu. Wynik przedstaw w postaci tablicy json zgodnie ze schematem " + actors_with_description_json + ", wartości pól uzupełnij w języku polskim."
+
+class RiskModule(modules.Module):
+    def __init__(self, model):
+        self.Model = model
+
+    def make_ai_call(self, message, type):
+        messages = [{"role": "system", "content": message}]
+        response = utils.sendAIRequest(self.Model, messages, "json", 4000)
+        return response
+
+    async def get_content(self, forWho, doingWhat):
+        text_response_for_risk = self.make_ai_call(query_for_who + " " + forWho + " " + query_doing_what + " " + doingWhat + " " + query_expectations, {"type": "json_object"});
+        
+        with open('risk.json', 'w') as file:
+            file.write(text_response_for_risk.choices[0].message.content)
+
+        return text_response_for_risk
