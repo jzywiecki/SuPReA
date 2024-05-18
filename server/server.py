c863ab2769e2ee3f10b543c624853fbbc3b80863
jzywiecki <jakubzywiecki1@gmail.com>
2024-05-18 14:39:07 +0200

    Add requirements module
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ import modules.risk_module.routes as risks
 import modules.specifications_module.routes as specifications
 import modules.strategy_module.routes as strategy
 import modules.motto_module.routes as motto_module
+import modules.requirements_module.routes as requirements_module
 import utils.openaiUtils as utils 
 # ----------------------------
 
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     specifications = specifications.SpecificationsModule(utils.Model.GPT3)
     strategies = strategy.StrategyModule(utils.Model.GPT3)
     motto = motto_module.MottoModule(utils.Model.GPT3)
+    requirements = requirements_module.RequirementsModule(utils.Model.GPT3)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(actors.get_content("startupu", "wyprowadzania psów"))
@@ -29,6 +31,8 @@ if __name__ == "__main__":
     loop.run_until_complete(specifications.get_content("startupu", "wyprowadzania psów"))
     loop.run_until_complete(strategies.get_content("startupu", "wyprowadzania psów"))
     loop.run_until_complete(motto.get_content("startupu", "wyprowadzania psów"))
+    loop.run_until_complete(requirements.get_content("startupu", "wyprowadzania psów"))
+
 
     #logger.configure_logging()
     #asyncio.run(server())
