862ada93d69f1658b09f8fae24691947d8af96a7
bartipablo <bartek.pawlowski00@gmail.com>
2024-07-09 19:47:39 +0200

    dynamic scrolling of messages

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,49 +1,98 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ChatMessage from "@/components/ChatMessage";
 import { Button } from "./ui/button";
-import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
+import * as ScrollArea from '@radix-ui/react-scroll-area';
+import React from "react";
+import { useEffect, useState } from "react";
 
-const Chat = ({ isCollapsed }) => {
+const Chat = ({ isCollapsed, projectId, senderId }) => {
+
+    const [connected, setConnected] = useState(false);
+    const [messages, setMessages] = useState([]);
+
+    // max constraint height
+    const [scrollChatHeight, setScrollChatHeight] = useState(0); 
+    const [scrollAiChatHeight, setScrollAiChatHeight] = useState(0);
+
+    // current scroll Y position
+    const [scrollChatTop, setScrollChatTop] = useState(0);
+    const [scrollAiChatTop, setScrollAiChatTop] = useState(0);
+
+    // height of visible area
+    const [clientChatHeight, setClientChatHeight] = useState(0);
+    const [clientAiChatHeight, setClientAiChatHeight] = useState(0);
+
+    
+    function useChatScroll<T>(dep: T): React.MutableRefObject<HTMLDivElement> {
+        const ref = React.useRef<HTMLDivElement>();
+        React.useEffect(() => {
+          if (ref.current && scrollAiChatHeight - scrollAiChatTop == clientAiChatHeight) {
+            ref.current.scrollTop = ref.current.scrollHeight;
+          }
+        }, [dep]);
+        return ref;
+    }
+
+    const scrollAiRef = useChatScroll(messages);
+
 
     return (
         <div className="h-full w-full flex-col justify-center">
             {isCollapsed && (
-            <>
-            <Tabs defaultValue="ai">
-            <TabsList className="grid w-full grid-cols-2 h-10">
-                <TabsTrigger value="ai">AI Chat</TabsTrigger>
-                <TabsTrigger value="discussion">Discussion</TabsTrigger>
-            </TabsList>
-            <TabsContent value="ai">
-            <ScrollArea style={{ height: 'calc(100vh - 68*4px)'}}>
-                    <ChatMessage message={{user: "GPT", content: "What do you need help with?"}} id={3} />
-                    <ChatMessage message={{user: "User", content: "I need help with my project"}} id={4} />    
-                    <ChatMessage message={{user: "GPT", content: "What do you need help with?"}} id={3} />
-                    <ChatMessage message={{user: "User", content: "I need help with my project"}} id={4} />
-                    <ChatMessage message={{user: "GPT", content: "What do you need help with?"}} id={3} />
-                    <ChatMessage message={{user: "User", content: "I need help with my project"}} id={4} />
-                    <ChatMessage message={{user: "GPT", content: "Hello, how can I help you?"}} id={1} />
-                    <ChatMessage message={{user: "User", content: "I need help with my project"}} id={2} />
-                </ScrollArea>
-            </TabsContent>
-            <TabsContent value="discussion">
-a
-            </TabsContent>
-            </Tabs>
-            
-            <div className="w-full flex items-center bg-muted h-18">
-                <Input placeholder="Write your message here" className="m-2"/>
-                <Button className="m-2">Send message</Button>
-            </div>
-            <div className="bg-muted text-center text-sm text-muted-foreground p-2 h-18">
-            GPT might make mistakes. 
-            </div>
-            </>
+                <>
+                    <Tabs defaultValue="ai">
+                        <TabsList className="grid w-full grid-cols-2 h-10">
+                            <TabsTrigger value="ai">AI Chat</TabsTrigger>
+                            <TabsTrigger value="discussion">Discussion</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="ai">
+
+                        <ScrollArea.Root className="scroll-area-root" style={{ height: 'calc(92vh - 68*4px)' }}>
+                            <ScrollArea.Viewport className="scroll-area-viewport" 
+                                ref={scrollAiRef} 
+                                style={{ height: 'calc(92vh - 68*4px)' }}
+                                onScrollCapture={(event) => {
+                                    setScrollAiChatHeight(event.target.scrollHeight);
+                                    setScrollAiChatTop(event.target.scrollTop);
+                                    setClientAiChatHeight(event.target.clientHeight);
+                                }}>
+                                {messages.map((message) => (
+                                    <ChatMessage key={message.id} message={message} id={message.id} />
+                                ))}
+                            </ScrollArea.Viewport>
+                            <ScrollArea.Scrollbar className="scroll-area-scrollbar" orientation="vertical">
+                                <ScrollArea.Thumb className="scroll-area-thumb" />
+                            </ScrollArea.Scrollbar>
+                            <ScrollArea.Corner className="scroll-area-corner" />
+                        </ScrollArea.Root>
+
+
+                            </TabsContent>
+                        <TabsContent value="discussion">
+                            <ScrollArea onScrollCapture={ (event) => {
+                                    setScrollChatHeight(event.target.scrollHeight);
+                                    setScrollChatTop(event.target.scrollTop);
+                                    setClientChatHeight(event.target.clientHeight);
+                            }} 
+                                style={{ height: 'calc(92vh - 68*4px)'} 
+                            }>
+                                siema
+                            </ScrollArea>
+                        </TabsContent>
+                    </Tabs>
+                    
+                    <div className="w-full flex items-center bg-muted ">
+                        <Textarea placeholder="Aa" />
+                        <Button className="m-2">Send message</Button>
+                    </div>
+                    <div className="bg-muted text-center text-sm text-muted-foreground p-2 h-18">
+                        {connected ? "Connected" : "No connection."}
+                    </div>
+                </>
             )}
-            
         </div>
-    )
+    );
 }
 
 export default Chat;
--- client/src/components/ChatMessage.tsx
+++ client/src/components/ChatMessage.tsx
@@ -6,7 +6,8 @@ import {
 
 
 const ChatMessage = ({ message, id }) => {
-    
+
+
     return (
         <div className={`flex p-3 ${id % 2 == 1 ? 'justify-start bg-accent' : 'justify-end bg-background'}`}>
             <Avatar className="m-2">
@@ -16,6 +17,9 @@ const ChatMessage = ({ message, id }) => {
             <div className="flex-col">
             <div className="font-bold">{message.user}</div>
             <div className="">{message.content}</div>
+            {!message.confirmed ? (
+                    <div className="text-sm text-gray-500">Sending...</div>
+            ) : null}
             </div>
         </div>
     )
