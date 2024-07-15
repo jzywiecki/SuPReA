a083e4b13452c892f9e75fa29fbe0e21b3b12eeb
bartipablo <bartek.pawlowski00@gmail.com>
2024-07-15 16:45:07 +0200

    update routers: removed synchronous endpoints to generate modules and improved get modules endpoints

--- server/routers/elevator_speech.py
+++ server/routers/elevator_speech.py
@@ -1,11 +1,6 @@
-from fastapi import APIRouter, HTTPException
-from bson import ObjectId
-from server.models import ElevatorSpeechModel
-from server.database import project_collection
-from server.modules.elevator_speech.routes import ElevatorSpeechModule
-from server.utils.openaiUtils import Model
-from pymongo import ReturnDocument
-import json
+from fastapi import APIRouter
+from routers.projects import get_module
+
 
 router = APIRouter(
     tags=["elevator_speech"],
@@ -18,38 +13,4 @@ router = APIRouter(
     "/{project_id}",
 )
 async def get_elevator_speeches(project_id: str):
-    if (
-        project := await project_collection.find_one({"_id": ObjectId(project_id)})
-    ) is not None:
-        elevator_speeches = project["elevator_speech"]
-        return elevator_speeches
-    else:
-        raise HTTPException(status_code=404, detail=f"Project {project_id} not found")
-
-
-@router.post("/generate/{project_id}")
-async def generate_elevator_speech(project_id: str):
-    print(f"Generating elevator speech for project {project_id}")
-    project = await project_collection.find_one({"_id": ObjectId(project_id)})
-    if project:
-        try:
-            elevator_speech = ElevatorSpeechModule(Model.GPT3)
-            for_who = project["for_who"]
-            doing_what = project["doing_what"]
-            additional_info = project["additional_info"]
-            content = elevator_speech.get_content(
-                for_who, doing_what, additional_info, False
-            )
-            data = json.loads(content.choices[0].message.content)
-            elevator_speech_model = ElevatorSpeechModel(**data)
-            project["elevator_speech"] = elevator_speech_model.dict()
-            await project_collection.find_one_and_update(
-                {"_id": ObjectId(project_id)},
-                {"$set": project},
-                return_document=ReturnDocument.AFTER,
-            )
-            return elevator_speech_model
-        except Exception as e:
-            print("Error in generating elevator speech!")
-            return
-    raise HTTPException(status_code=404, detail=f"Project {project_id} not found")
+    return await get_module(project_id, "elevator_speech")
--- server/routers/project_schedule.py
+++ server/routers/project_schedule.py
@@ -1,11 +1,6 @@
-from fastapi import APIRouter, HTTPException
-from bson import ObjectId
-from server.models import ProjectScheduleModel
-from server.database import project_collection
-from server.modules.project_schedule.routes import ScheduleModule
-from server.utils.openaiUtils import Model
-from pymongo import ReturnDocument
-import json
+from fastapi import APIRouter
+from routers.projects import get_module
+
 
 router = APIRouter(
     tags=["project_schedule"],
@@ -18,38 +13,4 @@ router = APIRouter(
     "/{project_id}",
 )
 async def get_project_schedule(project_id: str):
-    if (
-        project := await project_collection.find_one({"_id": ObjectId(project_id)})
-    ) is not None:
-        project_schedule = project["project_schedule"]
-        return project_schedule
-    else:
-        raise HTTPException(status_code=404, detail=f"Project {project_id} not found")
-
-
-@router.post("/generate/{project_id}")
-async def generate_project_schedule(project_id: str):
-    print(f"Generating project schedule for project {project_id}")
-    project = await project_collection.find_one({"_id": ObjectId(project_id)})
-    if project:
-        try:
-            project_schedule = ScheduleModule(Model.GPT3)
-            for_who = project["for_who"]
-            doing_what = project["doing_what"]
-            additional_info = project["additional_info"]
-            content = project_schedule.get_content(
-                for_who, doing_what, additional_info, False
-            )
-            data = json.loads(content.choices[0].message.content)
-            project_schedule_model = ProjectScheduleModel(**data)
-            project["project_schedule"] = project_schedule_model.dict()
-            await project_collection.find_one_and_update(
-                {"_id": ObjectId(project_id)},
-                {"$set": project},
-                return_document=ReturnDocument.AFTER,
-            )
-            return project_schedule_model
-        except Exception as e:
-            print("Error in generating project schedule!")
-            return
-    raise HTTPException(status_code=404, detail=f"Project {project_id} not found")
+    return await get_module(project_id, "project_schedule")
