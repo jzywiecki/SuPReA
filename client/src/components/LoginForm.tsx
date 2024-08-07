c7560dbb1d5e7f48d48e3efad65995e93188a154
jzywiecki <jakubzywiecki1@gmail.com>
2024-08-07 21:18:27 +0200

    login and registration forms work with dark mode
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -64,7 +64,7 @@ const LoginForm: React.FC = () => {
     };
 
     return (
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
             <div
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true"
@@ -78,7 +78,7 @@ const LoginForm: React.FC = () => {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Login</h2>
+                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Login</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
                     Please enter your email and password to login.
                 </p>
@@ -86,7 +86,7 @@ const LoginForm: React.FC = () => {
             <div className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div className="sm:col-span-2">
-                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6">
                             Email
                         </label>
                         <div className="mt-2.5">
@@ -95,7 +95,7 @@ const LoginForm: React.FC = () => {
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="password" className="block text-sm font-semibold leading-6">
                             Password
                         </label>
                         <div className="mt-2.5">
--- client/src/components/RegistrationForm.tsx
+++ client/src/components/RegistrationForm.tsx
@@ -99,7 +99,7 @@ const RegisterForm: React.FC = () => {
     };
 
     return (
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
             <div
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true"
@@ -113,7 +113,7 @@ const RegisterForm: React.FC = () => {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Register</h2>
+                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Register</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
                     Please enter your name, email and password to register.
                 </p>
@@ -121,7 +121,7 @@ const RegisterForm: React.FC = () => {
             <div className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div className="sm:col-span-2">
-                    <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">
+                    <label htmlFor="name" className="block text-sm font-semibold leading-6 ">
                             username
                         </label>
                         <div className="mt-2.5">
@@ -130,7 +130,7 @@ const RegisterForm: React.FC = () => {
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6 ">
                             Email
                         </label>
                         <div className="mt-2.5">
@@ -139,7 +139,7 @@ const RegisterForm: React.FC = () => {
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="password" className="block text-sm font-semibold leading-6 ">
                             Password
                         </label>
                         <div className="mt-2.5">
@@ -148,7 +148,7 @@ const RegisterForm: React.FC = () => {
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="confirm-password" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="confirm-password" className="block text-sm font-semibold leading-6">
                             Confirm Password
                         </label>
                         <div className="mt-2.5">
