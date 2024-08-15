279a11c7255a99e4c80bb5c070d424b69c7ee8f0
jzywiecki <jakubzywiecki1@gmail.com>
2024-08-15 15:07:23 +0200

    minor ux fixes, backend changes
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- client/src/components/NewProjectInput.tsx
+++ client/src/components/NewProjectInput.tsx
@@ -147,7 +147,7 @@ const NewProjectInput = () => {
     }
 
     return (
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
             <div
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true"
@@ -161,7 +161,7 @@ const NewProjectInput = () => {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Create new project</h2>
+                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Create new project</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
                     You can use AI or create an empty project.
                 </p>
@@ -170,7 +170,7 @@ const NewProjectInput = () => {
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
 
                     <div className="sm:col-span-2">
-                        <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="company" className="block text-sm font-semibold leading-6">
                             Name
                         </label>
                         <div className="mt-2.5">
@@ -188,7 +188,7 @@ const NewProjectInput = () => {
                 </div>
                 {usedAi && <div>
                     <div className="sm:col-span-2 mt-2.5">
-                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="message" className="block text-sm font-semibold leading-6">
                             For who?
                         </label>
                         <div className="mt-2.5">
@@ -198,7 +198,7 @@ const NewProjectInput = () => {
                         </div>
                     </div>
                     <div className="sm:col-span-2 mt-2.5">
-                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="message" className="block text-sm font-semibold leading-6">
                             Doing what?
                         </label>
                         <div className="mt-2.5">
@@ -209,7 +209,7 @@ const NewProjectInput = () => {
                     </div>
 
                     <div className="sm:col-span-2 mt-2.5">
-                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="message" className="block text-sm font-semibold leading-6">
                             Additional information?
                         </label>
                         <div className="mt-2.5">
@@ -225,7 +225,7 @@ const NewProjectInput = () => {
                     <Button
                         onClick={() => submitButton()}
                         type="submit"
-                        className="block w-full rounded-md  px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="block w-full rounded-md  px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Generate
                     </Button>
