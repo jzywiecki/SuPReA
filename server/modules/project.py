f45962db1fd09f9cac1f943dca18b0cec6fc66ce
bartipablo <bartek.pawlowski00@gmail.com>
2024-08-31 22:23:18 +0200

    add new create_empty_project method

--- server/modules/project.py
+++ server/modules/project.py
@@ -1,33 +1,31 @@
-async def create_project(
-    project_name, owner_id, for_who: str, doing_what: str, additional_info: str
+from datetime import datetime
+from models import Chat, Project
+from bson import ObjectId
+from database import chats_dao, projects_dao
+
+
+def create_empty_project(
+        project_name, owner_id, description: str, for_who: str, doing_what: str, additional_info: str
 ):
     discussion_chat = Chat(last_message_id=0, messages=[])
-
     ai_chat = Chat(last_message_id=0, messages=[])
 
-    discussion_chat_id = await chats_collection.insert_one(
-        discussion_chat.model_dump(by_alias=True, exclude=["id"])
-    )
+    discussion_chat_id = chats_dao.save_chat(discussion_chat).inserted_id
+    ai_chat_id = chats_dao.save_chat(ai_chat).inserted_id
 
-    ai_chat = await chats_collection.insert_one(
-        ai_chat.model_dump(by_alias=True, exclude=["id"])
-    )
-
-    new_project = ProjectModel(
+    new_project = Project(
         name=project_name,
         for_who=for_who,
         doing_what=doing_what,
         additional_info=additional_info,
+        description=description,
         owner=ObjectId(owner_id),
         members=[ObjectId(owner_id)],
-        description="",
         created_at=datetime.now(),
-        chat_id=ObjectId(discussion_chat_id.inserted_id),
-        ai_chat_id=ObjectId(ai_chat.inserted_id),
+        chat_id=ObjectId(discussion_chat_id),
+        ai_chat_id=ObjectId(ai_chat_id),
     )
 
-    result = await project_collection.insert_one(
-        new_project.model_dump(by_alias=True, exclude=["id"])
-    )
+    result = projects_dao.save_project(new_project)
 
     return str(result.inserted_id)
