85f5ea2e4fc28b5451b5c5b6b4b39f2558cf65e9
bartipablo <bartek.pawlowski00@gmail.com>
2024-09-01 13:29:58 +0200

    added actor to generate project

--- server/modules/project.py
+++ server/modules/project.py
@@ -1,31 +1,58 @@
-from datetime import datetime
-from models import Chat, Project
-from bson import ObjectId
-from database import chats_dao, projects_dao
+import ray
+
+from .actors import ActorsModule
+from .business_scenarios import BusinessScenariosModule
+from .database_schema import DatabaseSchemaModule
+from .elevator_speech import ElevatorSpeechModule
+from .logo import LogoModule
+from .motto import MottoModule
+from .project_schedule import ProjectScheduleModule
+from .requirements import RequirementsModule
+from .risks import RiskModule
+from .specifications import SpecificationsModule
+from .strategy import StrategyModule
+from .title import TitleModule
 
 
-def create_empty_project(
-        project_name, owner_id, description: str, for_who: str, doing_what: str, additional_info: str
-):
-    discussion_chat = Chat(last_message_id=0, messages=[])
-    ai_chat = Chat(last_message_id=0, messages=[])
+@ray.remote
+class ProjectAIGenerationActor:
+    def __init__(self):
+        self.actors_actor = ActorsModule.remote()
+        self.business_scenarios_actor = BusinessScenariosModule.remote()
+        self.database_schema_actor = DatabaseSchemaModule.remote()
+        self.elevator_speech_actor = ElevatorSpeechModule.remote()
+        self.logo_actor = LogoModule.remote()
+        self.motto_actor = MottoModule.remote()
+        self.project_schedule_actor = ProjectScheduleModule.remote()
+        self.requirements_actor = RequirementsModule.remote()
+        self.risks_actor = RiskModule.remote()
+        self.specifications_actor = SpecificationsModule.remote()
+        self.strategy_actor = StrategyModule.remote()
+        self.title_actor = TitleModule.remote()
+        self.actors = [
+            self.actors_actor,
+            self.business_scenarios_actor,
+            self.database_schema_actor,
+            self.elevator_speech_actor,
+            self.logo_actor,
+            self.motto_actor,
+            self.project_schedule_actor,
+            self.requirements_actor,
+            self.risks_actor,
+            self.specifications_actor,
+            self.strategy_actor,
+            self.title_actor,
+        ]
 
-    discussion_chat_id = chats_dao.save_chat(discussion_chat).inserted_id
-    ai_chat_id = chats_dao.save_chat(ai_chat).inserted_id
+    def generate_components_by_ai(self, ai_model, for_what, doing_what, additional_info):
+        for actor in self.actors:
+            actor.generate_by_ai.remote(ai_model, for_what, doing_what, additional_info)
 
-    new_project = Project(
-        name=project_name,
-        for_who=for_who,
-        doing_what=doing_what,
-        additional_info=additional_info,
-        description=description,
-        owner=ObjectId(owner_id),
-        members=[ObjectId(owner_id)],
-        created_at=datetime.now(),
-        chat_id=ObjectId(discussion_chat_id),
-        ai_chat_id=ObjectId(ai_chat_id),
-    )
-
-    result = projects_dao.save_project(new_project)
-
-    return str(result.inserted_id)
+    # TODO: below method can throw exception. Handle it.
+    def save_components_to_database(self, project_id):
+        for actor in self.actors:
+            value_ref = actor.get_value.remote()
+            value = ray.get(value_ref)
+            if value is not None:
+                break
+            actor.save_to_database.remote(project_id)
