d9ed454dca306638956d21f620b2ac8de60fa846
bartipablo <bartek.pawlowski00@gmail.com>
2024-09-13 17:08:32 +0200

    add gpt4o and dalle2 models

--- server/ai/__init__.py
+++ server/ai/__init__.py
@@ -5,7 +5,11 @@ from .open_ai import GPT35Turbo
 from .open_ai import DallE3
 
 from .open_ai import gpt_35_turbo
+from .open_ai import gpt_4o_mini
+from .open_ai import dall_e_2
 from .open_ai import dall_e_3
 
 from .open_ai import gpt_35_turbo_remote_ref
+from .open_ai import gpt_4o_mini_remote_ref
+from .open_ai import dall_e_2_remote_ref
 from .open_ai import dall_e_3_remote_ref
