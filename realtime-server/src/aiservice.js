49caa99c0cf6f71ac4d71e21eda78f5c4fa53246
bartipablo <bartek.pawlowski00@gmail.com>
2024-10-14 16:50:15 +0200

    add realtime-server aiservice prototype

---
+++ realtime-server/src/aiservice.js
@@ -0,0 +1,27 @@
+export class AIService {
+    constructor(editionService) {
+        this.editionService = editionService;
+    }
+
+
+    notifyMessageCompleted(message) {
+
+    }
+
+    
+    notifyGenerationCompleted(result) {
+    }
+
+
+    notfiyRegenerationCompleted(result) {
+
+    }
+
+
+    notifyUpdateCompleted(result) {
+    }
+
+
+    notifyComponentGeneration(result, eventName) {
+    }
+}
