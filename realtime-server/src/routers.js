08df7bcdde34d02b5cf17199e4765ab998df2cbf
bartipablo <bartek.pawlowski00@gmail.com>
2024-10-14 16:49:45 +0200

    add realtime-server routers prototype

---
+++ realtime-server/src/routers.js
@@ -0,0 +1,18 @@
+export const registerRouters = (app) => {
+
+    app.post('/event/generation-complete', (req, res) => {
+    });
+    
+
+    app.post('/event/regeneration-complete', (req, res) => {
+    });
+
+
+    app.post('/event/update-complete', (req, res) => {
+    });
+    
+
+    app.post('/event/message', (req, res) => {
+    });
+
+}
