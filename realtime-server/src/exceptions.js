17eeccd7fdbc8330f8bbd71b6f27f4da8b32f60f
bartipablo <bartek.pawlowski00@gmail.com>
2024-10-14 23:35:37 +0200

    refactor exceptions

--- realtime-server/src/exceptions.js
+++ realtime-server/src/exceptions.js
@@ -14,17 +14,33 @@ export class InternalServerError extends Error {
 }
 
 
-export class ComponentIsNotExist extends Error {
+export class ComponentIsNotExistException extends Error {
     constructor(message = 'Component does not exist.') {
         super(message);
-        this.name = 'ModelsIsNotExist';
+        this.name = 'ModelsIsNotExistException';
     }
 }
 
 
-export class ComponentIsAlreadyEdited extends Error {
+export class ComponentIsAlreadyEditedException extends Error {
     constructor(message = 'Component is already being edited.') {
         super(message);
-        this.name = 'ComponentIsAlreadyEdited';
+        this.name = 'ComponentIsAlreadyEditedException';
+    }
+}
+
+
+export class InvalidArgumentException extends Error {
+    constructor(message = 'Invalid argument.') {
+        super(message);
+        this.name = 'InvalidArgumentException';
+    }
+}
+
+
+export class SessionIsNotBeingEditedException extends Error {
+    constructor(message = 'Session is not being edited.') {
+        super(message);
+        this.name = 'SessionIsNotBeingEditedException';
     }
 }
