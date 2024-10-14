f0a4c9e84cc7f8d02c8e2fd5072d757203e9a021
bartipablo <bartek.pawlowski00@gmail.com>
2024-10-14 20:22:27 +0200

    edition service skeleton

---
+++ realtime-server/src/edition.js
@@ -0,0 +1,10 @@
+export const registerEditionEvents = (socket, session, editionRegister) => {
+    
+    socket.on('edit-component', (component) => {
+    });
+}
+
+
+export const transmitEditionsStatusOnConnection = async (socket, editionRegister) => {
+
+};
