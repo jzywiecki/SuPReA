eaa6b94f8416ac9af0e7f7432d57aa46c455f7b9
jzywiecki <jakubzywiecki1@gmail.com>
2024-10-25 18:19:24 +0200

    black linter
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- server/database/projects.py
+++ server/database/projects.py
@@ -32,7 +32,6 @@ class ProjectDAO:
         project = self.collection.find_one({"_id": ObjectId(project_id)})
         print(project)
         return project
-        
 
     def get_projects_by_owner(self, owner_id: str) -> List[Dict]:
         """
