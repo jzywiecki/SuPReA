32830e03084a2e97122c7327e86481f84e97fa28
bartipablo <bartek.pawlowski00@gmail.com>
2024-10-26 10:10:40 +0200

    rename socket

--- client/src/sockets.tsx
+++ client/src/sockets.tsx
@@ -2,9 +2,7 @@ import { io } from 'socket.io-client';
 
 const URL = 'http://localhost:3333';
 
-const socketChats = io(URL, {
+export const socket = io(URL, {
     autoConnect: false,
     path: "/realtime-server/socket.io/"
 });
-
-export { socketChats };
