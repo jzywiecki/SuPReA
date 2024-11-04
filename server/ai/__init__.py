8b95c6f5695e4e1cf54e18afb713587c30d03af6
jzywiecki <jakubzywiecki1@gmail.com>
2024-11-04 21:35:25 +0100

    add local ai model on server
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- server/ai/__init__.py
+++ server/ai/__init__.py
@@ -14,6 +14,10 @@ from .open_ai import gpt_4o_mini_remote_ref
 from .open_ai import dall_e_2_remote_ref
 from .open_ai import dall_e_3_remote_ref
 
+from .local_ai import llama_32
+
+from .local_ai import llama_32_remote_ref
+
 from .model_enum import get_model_remote_ref_enum
 from .model_enum import get_image_model_remote_ref_enum
 from .model_enum import get_text_model_remote_ref_enum
