0e1eb6313b3ed5eea1f6db4cd84fa42c10f6adbe
jzywiecki <jakubzywiecki1@gmail.com>
2024-11-10 22:48:42 +0100

    sockets fix of path
    Signed-off-by: jzywiecki <jakubzywiecki1@gmail.com>

--- client/src/utils/sockets.ts
+++ client/src/utils/sockets.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 import { API_URLS } from "@/services/apiUrls";
 
-export const socket = io(API_URLS.REALTIME_SERVER_URL, {
+export const socket = io(API_URLS.BASE_URL, {
     autoConnect: false,
+    path: "/realtime-server/socket.io"
 });
