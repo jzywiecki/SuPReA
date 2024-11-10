6f1e28f6e9f7f0982a85deb946762e6768ae0727
Jakub Zywiecki - Windows PC <jakubzywiecki1@gmail.com>
2024-11-10 01:55:07 +0100

    Api urls now works with nginx

--- client/src/services/apiUrls.tsx
+++ client/src/services/apiUrls.tsx
@@ -1,8 +1,9 @@
-
 const API_GATEWAY_URL = 'http://localhost:3333';
+const NGINX_API_GATEWAY_URL = '/api';
+const NGINX = process.env.NGINX === 'TRUE'; 
 
 export const API_URLS = {
-    BASE_URL: API_GATEWAY_URL,
-    API_SERVER_URL: `${API_GATEWAY_URL}/server`,
-    REALTIME_SERVER_URL: `${API_GATEWAY_URL}/realtime-server`
+    BASE_URL: NGINX ? NGINX_API_GATEWAY_URL : API_GATEWAY_URL,
+    API_SERVER_URL: `${NGINX ? NGINX_API_GATEWAY_URL : API_GATEWAY_URL}/server`,
+    REALTIME_SERVER_URL: `${NGINX ? NGINX_API_GATEWAY_URL : API_GATEWAY_URL}/realtime-server`
 } as const;
