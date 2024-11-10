55f638c1acd4983e1df2314a7ab2f95a2ae1b875
jzywiecki <jakubzywiecki1@gmail.com>
2024-11-10 22:24:37 +0100

    change localhost to realtime server url

--- client/src/utils/sockets.ts
+++ client/src/utils/sockets.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3000';
+const URL = 'http://realtime-server:3000';
 
 export const socket = io(URL, {
     autoConnect: false,
