c18e042c6d250ace8806951245a704679a5edd7c
bartipablo <bartek.pawlowski00@gmail.com>
2024-11-11 17:08:37 +0100

    add auto scroll in create project

--- client/src/pages/CreateProject.tsx
+++ client/src/pages/CreateProject.tsx
@@ -1,13 +1,21 @@
-// import NewProjectInput from "@/components/NewProjectInput"
+import { useEffect } from "react";
 import NewProjectInput from "@/components/NewProjectInput";
 
 const CreateProject = () => {
 
-    return (
-        <>
-            <NewProjectInput />
-        </>
-    )
-}
+useEffect(() => {
+    window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth", // Dodajemy płynne przewijanie
+    });
+}, []);
+
+  return (
+    <>
+      <NewProjectInput />
+    </>
+  );
+};
 
 export default CreateProject;
