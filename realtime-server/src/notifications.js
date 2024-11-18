def6b60bbb9e12b9c76cedb0e9dad1fc23670a53
bartipablo <bartek.pawlowski00@gmail.com>
2024-11-18 18:47:42 +0100

    rebuild realtime server edition

--- realtime-server/src/notifications.js
+++ realtime-server/src/notifications.js
@@ -4,21 +4,6 @@
  */
 
 
-export class ComponentGeneratedCommunicate {
-    /**
-     * Message representing a response from the AI to a regeneration/update request.
-     * Sent to the specific client that made the request.
-     * 
-     */
-    code = 0;
-
-    constructor(componentId, value) {
-        this.component = componentId;
-        this.value = value;
-    }
-}
-
-
 export class ComponentCreatedCommunicate {
     /**
      * Notification sent when the AI completes the generation of a component for the first time.
@@ -29,65 +14,6 @@ export class ComponentCreatedCommunicate {
     constructor(componentId) {
         this.component = componentId;
     }
-}
-
-
-export class RegisterEditSessionCommunicate {
-    /**
-     * Notification informing about the start of a new editing session.
-     * Sent to all project members.
-     */
-    code = 2;
-    
-    constructor(componentId, userId) {
-        this.component = componentId;
-        this.userId = userId;
-    }
-}
-
-
-export class UnregisterEditSessionCommunicate {
-    /**
-     * Notification informing about the end of an editing session.
-     * Sent to all project members.
-     */
-    code = 3;
-
-    constructor(componentId, userId) {
-        this.component = componentId;
-        this.userId = userId;
-    }
-}
-
-
-export class EditSessionIsNotActiveCommunicate {
-    /**
-     * Notification informing about the rejection of an edit (update, regenerate by ai, update by ai) request.
-     * Sent to the client that made the request.
-     */
-    code = 7;
-
-    constructor(details) {
-        this.details = details;
-    }
-}
-
-
-export class ConfirmedUpdateRequestCommunicate {
-    /**
-     * Notification informing about the confirmation of an update request.
-     * Sent to the client that made the request.
-     */
-    code = 8;
-}
-
-
-export class ConfirmedForwardRequestToAICommunicate {
-    /**
-     * Notification informing about the confirmation of a request to forward the update to the AI.
-     * Sent to the client that made the request.
-     */
-    code = 9;
 }
 
 
--- realtime-server/src/register.js
+++ realtime-server/src/register.js
@@ -5,8 +5,7 @@
 import { isComponentIdCorrect } from "./model.js";
 import { 
     ComponentIsNotExistException,
-    SessionIsNotRegisteredException,
-    UserAlreadyHasActiveEditSessionException
+    SessionIsNotRegisteredException
  } from "./exceptions.js";
 
 
@@ -20,7 +19,6 @@ class ProjectEditionsRegister {
     constructor() {
         this.mapComponentToSessions = new Map();
         this.mapSessionIdToSession = new Map();
-        this.activeUsersEditorsSet = new Set();
         this.activeSessionsQuantity = 0;
     }
 
@@ -38,10 +36,6 @@ class ProjectEditionsRegister {
             throw new ComponentIsNotExistException("Invalid component ID.");
         }
     
-        // Check if the user already has an active session
-        if (this.activeUsersEditorsSet.has(session.userId)) {
-            throw new UserAlreadyHasActiveEditSessionException("User already has an active edit session.");
-        }
     
         // Initialize the component's session array if it doesn't exist
         if (!this.mapComponentToSessions.has(componentId)) {
@@ -51,27 +45,26 @@ class ProjectEditionsRegister {
         // Add the session to component's session list and to global session maps
         this.mapComponentToSessions.get(componentId).push(session);
         this.mapSessionIdToSession.set(session.id, session);
-        this.activeUsersEditorsSet.add(session.userId);
     
         // Increment the active session count
         this.activeSessionsQuantity++;
     }
     
 
-    removeSession(session) {
+    removeSession(sessionId) {
         /**
         * Removes an active editing session for a component.
         * 
         * @param {Object} session - The session object to be removed.
         * @throws {SessionIsNotRegisteredException} If the session is not registered.
         */
-        if (!this.isSessionRegistered(session.id)) {
+        if (!this.isSessionRegistered(sessionId.id)) {
             throw new SessionIsNotRegisteredException("Cannot remove a session that is not registered.");
         }
     
-        const componentId = this.getComponentForSession(session.id);
+        const componentId = this.getComponentForSession(sessionId.id);
         const sessions = this.mapComponentToSessions.get(componentId);
-        const sessionIndex = sessions.findIndex(s => s.id === session.id);
+        const sessionIndex = sessions.findIndex(s => s.id === sessionId.id);
     
         if (sessionIndex !== -1) {
             // Remove the session from the component's session array
@@ -84,8 +77,7 @@ class ProjectEditionsRegister {
         }
     
         // Remove session from global maps and active users set
-        this.mapSessionIdToSession.delete(session.id);
-        this.activeUsersEditorsSet.delete(session.userId);
+        this.mapSessionIdToSession.delete(sessionId.id);
     
         // Decrement the active session count
         this.activeSessionsQuantity--;
@@ -97,60 +89,29 @@ class ProjectEditionsRegister {
     }
 
 
-    isActiveParticularSessionForComponent(session, component) {
-        /**
-        * Checks if the provided session is the active editing session for the specified component.
-        * 
-        * @param {Object} session - The session object to check against the active session for the component.
-        * @param {Object} component - The component name for which the active editing session is being checked.
-        * @returns {boolean} True if the provided session is the active session for the component, false otherwise.
-        * @throws {Error} If the component does not exist in the project.
-        */
-        if (!isComponentIdCorrect(component)) {
-            throw new ComponentIsNotExistException();
-        }
-
-        if (!this.mapComponentToSessions.has(component)) {
-            return false;
-        }
-
-        return this.mapComponentToSessions.get(component).includes(session);
-    }
-
-
     getActiveSessions() {
         /**
-        * Retrieves a list of active editing sessions for all components.
+        * Retrieves a list of active editing sessions for all components,
+        * ensuring that each list of users contains unique user IDs.
         * 
-        * @returns {Array} An array of objects, each containing the component id and the sessions.
+        * @returns {Array} An array of objects, each containing the component id and the unique users.
         */
         const result = [];
     
         for (let [component, sessions] of this.mapComponentToSessions.entries()) {
-            const result_element = {"component": component, "users": []};
+            const result_element = { "component": component, "users": [] };
             if (component && sessions) {
+                const userSet = new Set();
                 for (let session of sessions) {
-                    result_element.users.push(
-                        session.userId
-                    );
+                    userSet.add(session.userId.toString());
                 }
+                result_element.users = Array.from(userSet);
                 result.push(result_element);
             }
         }
     
         return result;
-    }
-    
-
-    getSession(id) {
-        /**
-        * Retrieves the session object for the provided session ID.
-        * 
-        * @param {string} id - The ID of the user for which the session is being retrieved.
-        * @returns {Object} The session object for the provided user ID.
-        */
-        return this.mapSessionIdToSession.get(id);
-    }
+    }    
 
 
     isSessionRegistered(sessionId) {
@@ -199,11 +160,11 @@ export class EditionRegister {
          * @throws {Error} If the component does not exist or is already being edited.
         */
         
-        if (!this.register.has(session.projectId)) {
-            this.register.set(session.projectId, new ProjectEditionsRegister());
+        if (!this.register.has(session.projectId.toString())) {
+            this.register.set(session.projectId.toString(), new ProjectEditionsRegister());
         }        
 
-        this.register.get(session.projectId).addSession(session, componentId);
+        this.register.get(session.projectId.toString()).addSession(session, componentId);
     }
 
 
@@ -217,7 +178,7 @@ export class EditionRegister {
          * @throws {Error} If the session is not being edited.
          * 
         */
-        const projectRegister = this.register.get(session.projectId);
+        const projectRegister = this.register.get(session.projectId.toString());
     
         if (!projectRegister) {
             throw new SessionIsNotRegisteredException("Cannot remove a session that is not registered.");
@@ -226,7 +187,7 @@ export class EditionRegister {
         const result = projectRegister.removeSession(session);
 
         if (projectRegister.getActiveSessionsQuantity() === 0) {
-            this.register.delete(session.projectId);
+            this.register.delete(session.projectId.toString());
         }
 
         return result;
@@ -241,38 +202,11 @@ export class EditionRegister {
              * @param {string} projectId - The ID of the project for which active sessions are being retrieved.
              * @returns {Array} An array of objects, each containing the component name and the user ID of the active session.
             */
-        const projectRegister = this.register.get(projectId);
+        const projectRegister = this.register.get(projectId.toString());
         if (!projectRegister) {
             return [];
         }
         
         return projectRegister.getActiveSessions();
-    }
-
-
-    isEditionSessionActive(session, componentId) {
-        /**
-         * Checks whether a specific edition session is currently active for a given component in a project.
-         * 
-         * @param {Object} session - The session object representing the edition session to check.
-         * @param {Object} component - The component for which the session is being checked.
-         * @returns {boolean} True if the session is active for the given component, false otherwise.
-         * @throws {Error} If the component does not exist in the project.
-         */
-        const projectRegister = this.register.get(session.projectId);
-
-        if (!projectRegister) {
-            return false;
-        }
-
-        return projectRegister.isActiveParticularSessionForComponent(session, componentId);
-    }
-
-
-    getSession(sessionId, projetId) {
-        /**
-         * Retrieves the session object for the provided session ID and project ID.
-         */
-        return this.register.get(projetId)?.getSession(sessionId);
     }
 }
