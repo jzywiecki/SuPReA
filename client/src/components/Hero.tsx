625daf4213d007bc5d1afeb8ff4b1b97979e05fd
bartipablo <bartek.pawlowski00@gmail.com>
2024-11-20 17:27:34 +0100

    add logut handler in hero section

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useUser } from "./UserProvider";
 
 const Hero = ({ onLearnMoreClick }) => {
+  const { user } = useUser();
+
   return (
     <div>
       <div className="relative isolate px-6 lg:px-8">
@@ -30,7 +33,7 @@ const Hero = ({ onLearnMoreClick }) => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                to="/create-project"
+                to={user ? "/create-project" : "/login"} // Ścieżka zależna od stanu logowania
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
